8a8b10f0fba15be27adeebe9f7b18217facd694b
royFloresNyc <royfloresnyc@gmail.com>
2020-09-01 11:26:05 -0400

    Reformat activity time

--- app/models/camp_activity.rb
+++ app/models/camp_activity.rb
@@ -1,4 +1,15 @@
 class CampActivity < ApplicationRecord
     belongs_to :camper
     belongs_to :activity
+
+    def pretty_time
+        hour = self.time 
+        if hour == 0
+            "12:00 am"
+        elsif hour > 12
+            "#{hour -12}:00 pm"
+        else 
+            "#{hour}:00 am"
+        end 
+    end 
 end 
